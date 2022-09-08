cef96132d5ab2c7c96e7916718774442be568671
woonhak-kong <gamdekong@gmail.com>
2022-09-08 11:50:35 -0400

    initial commit

---
+++ Assets/[Scripts]/HelloWrold.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWrold : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("App Started!");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/[Scripts]/PlayerBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.position = new Vector2(transform.position.x+x * Time.deltaTime, transform.position.y + y * Time.deltaTime);
+    }
+}
